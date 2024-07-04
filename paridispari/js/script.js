6d2beb975f02926cadade0650b7ec29ea65ee2d0
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-05 00:10:09 +0200

    [UPD] Created cost for random number and cost for the sum

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -1,8 +1,14 @@
 function PariDispari() {
-    
+
     // SCEGLI PARI DISPARI
-    const sceltaUtente = prompt("Scegli 'pari' o 'dispari': ").toLowerCase();
+    const scelta_utente = prompt("Scegli 'pari' o 'dispari': ").toLowerCase();
   
     // INSERISCI UN NUMERO DA 1 A 5
-    let numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5: "));
+    let numero_utente = parseInt(prompt("Inserisci un numero da 1 a 5: "));
+
+    // GENERO UN NUMERO RANDOM DA 1 A 5
+    const numero_computer = Math.floor(Math.random() * 5) + 1;
+
+    // SOMMA DEI 2 NUMERI
+    const somma = numero_utente + numero_computer;
 }

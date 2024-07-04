28bd744eb4a237a31861eab4b64fc66faafbed84
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-04 16:27:14 +0200

    [UPD] Defined function and index

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// DEFINISCO UNA FUNZIONE CON PARAMETRO E CON VALORE DI RITORNO
+function verificaPalindromo(parola) {
+
+    // CONVERTO IN MINUSCOLO
+    parola = parola.toLowerCase();
+
+     // INDICI PER PER SCORRERE LA PAROLA
+    let i = 0;
+    let j = parola.length - 1;
+}

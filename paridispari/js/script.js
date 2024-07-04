76bf90b2730787628c8b7b14eac1cb65cdc52256
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-05 00:07:36 +0200

    [UPD] Created function and prompt for user

---
+++ paridispari/js/script.js
@@ -0,0 +1,8 @@
+function PariDispari() {
+    
+    // SCEGLI PARI DISPARI
+    const sceltaUtente = prompt("Scegli 'pari' o 'dispari': ").toLowerCase();
+  
+    // INSERISCI UN NUMERO DA 1 A 5
+    let numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5: "));
+}
